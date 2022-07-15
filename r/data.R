e096e96f5a5e53f7222a9917d8067e8a853a310a
pea13007 <apicellapv@gmail.com>
2022-07-15 15:00:47 -0600

    first commit

---
+++ r/data.R
@@ -0,0 +1,31 @@
+
+# skills <- tribble(
+#   ~area, ~skills,
+#   "Computer", "R, Snapgene Viewer, BLAST, Ensembl, Microsoft Office",
+#   "Laboratory", "Tissue Culture, Protoplasting, Nucleic acid isolation, gel electrophoresis, PCR, qPCR, experimental design"
+# )
+
+# work <- tribble(
+#   ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+#   "Research Associate", "Mydecine Innovations Group Inc.", "October", 2020, "April", 2022, "Denver, Colorado", "Conceived the design of intellectual property related to increasing the rate biosynthesis of secondary metabolites in a filamentous fungus to be used as a heterologous host. Listed as a co-inventor on the provisional patent.",
+#   "Research Associate", "Mydecine Innovations Group Inc.", "October", 2020, "April", 2022, "Denver, Colorado", "Designed and implemented experiments related to nucleic acid isolation, molecular identification of species, gene expression analysis, stimulation of defense compound biosynthesis, cloning of metabolite biosynthesis cassettes, protoplasting, and CRISPR-Cas9 genome editing.",
+#   "Research Associate", "Mydecine Innovations Group Inc.", "October", 2020, "April", 2022, "Denver, Colorado", "Created mutants of a filamentous fungus to serve as platforms for ectopic metabolite production",
+#   "Research Associate", "Mydecine Innovations Group Inc.", "October", 2020, "April", 2022, "Denver, Colorado", "Facilitated the acquisition of scientific equipment and overall operationality of a 7500 ft^2 laboratory",
+#   "Research Associate", "Mydecine Innovations Group Inc.", "October", 2020, "April", 2022, "Denver, Colorado", "Trained colleagues and one intern in fungal tissue culture, molecular biology techniques, and basic use of R for data visualization and statistical analysis",
+#   "Research Associate", "Mydecine Innovations Group Inc.", "October", 2020, "April", 2022, "Denver, Colorado", "Created R Markdown guides on how to implement data visualization and analysis in R",
+#   "Graduate Research Assistant", "University of Connecticut ", "August", 2018, "August", 2020, "Storrs, Connecticut", "Successfully defended thesis on genetic regulation of cannabinoid biosynthesis in Cannabis sativa.",
+#   "Graduate Research Assistant", "University of Connecticut ", "August", 2018, "August", 2020, "Storrs, Connecticut", "Designed and executed experiments in university greenhouses and commercial grow facilities.",
+#   "Graduate Research Assistant", "University of Connecticut ", "August", 2018, "August", 2020, "Storrs, Connecticut", "Implemented quantitative PCR to measure gene expression levels of genes in the cannabinoid biosynthesis pathway and quantified cannabinoid content using HPLC",
+#   "Graduate Research Assistant", "University of Connecticut ", "August", 2018, "August", 2020, "Storrs, Connecticut", "Analyzed and visualized data in R to facilitate comprehension in invited talks and manuscripts.",
+#   "Graduate Research Assistant", "University of Connecticut ", "August", 2018, "August", 2020, "Storrs, Connecticut", "Obtained controlled substances research licenses for laboratory, wrote standard operating procedures for the sampling, transport, and secure storage of controlled substances.",
+#   "Graduate Mentor", "University of Connecticut ", "August", 2018, "August", 2020,"Storrs, Connecticut", "Trained undergraduate and graduate students in molecular biology techniques.",
+#   "Graduate Mentor", "University of Connecticut ", "August", 2018, "August", 2020, "Storrs, Connecticut", "Mentored one student to win a grant, design and execute an experiment, and present findings at a university research forum.",
+#   "Teaching Assistant", "University of Connecticut ", "August", 2018, "August", 2020, "Storrs, Connecticut", "Lectured one laboratory section of woody plant identification. Led 15 students through university campus to teach students characteristics of plants. Graded assignments and recorded grades.",
+#   "Undergraduate Researcher", "University of Connecticut ", "August", 2016, "May", 2018, "Storrs, Connecticut", "Awarded $8000 in grant funding to phenotypically survey North America’s largest Aronia germplasm collection. ",
+#   "Undergraduate Researcher", "University of Connecticut ", "August", 2016, "May", 2018, "Storrs, Connecticut", "Evaluated sugar and acidity content of fruit juice from 120 Aronia genotypes, commonly referred to as chokeberry. Identified specific genotypes with desirable Brix°: titratable acidity ratios to be used as parentage for breeding varieties with more palatable fruits. ",
+#   "Undergraduate Researcher", "University of Connecticut ", "August", 2016, "May", 2018, "Storrs, Connecticut", "Presented findings in posters at national and international conferences."
+# )
+
+
+
+
